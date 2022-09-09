8833c579fdcb55dabe2905ee77546dacdaab168d
Hector <victornothector@gmail.com>
2022-09-09 10:18:31 +0100

    added App.tsx/Layout.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,81 @@
 import { useState, useEffect, useRef } from 'react'
 import axios from 'axios'
-import './Layout.css';
+import Layout from "./Layout";
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
-}
+const App = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [data, setData] = useState([])
+  const [activeData, setActiveData] = useState([])
+  const [nextLink, setnextLink] = useState('')
+  const [previousLink, setpreviousLink] = useState('')
+  // const [viewIndex, setViewIndex] = useState(1)
+  const viewIndex = useRef(1)
+  const dataRef = useRef(null)
+
+  const processData = async()=>{
+    const activeRecords: any = (Object.entries(dataRef.current));
+
+    // get paging data
+    const pagingData = (activeRecords[((activeRecords.length) - 1)])[1];
+    setnextLink(pagingData.next)
+    setpreviousLink(pagingData.previous)
+
+    // get data and re-adjust to 
+
+    if(activeRecords.length === 3){
+
+      // assuming it has a next or previous
+      if(viewIndex.current === 1){
+
+        // console.log( (activeRecords[0])[1] )
+        setActiveData( (activeRecords[0])[1] )
+      }else{
+        setActiveData( (activeRecords[1])[1] )
+      }
+    }else{
+
+      // assuming it only a next or previous
+      setActiveData( (activeRecords[0])[1] )
+    }
+    
+  }
+
+  const requestNewPage = async(url = "")=>{
+    try{
+
+      const res = await axios.get(url || ('https://randomapi.com/api/8csrgnjw?key=LEIX-GF3O-AG7I-6J84&page=1'))
+      
+      setData((res.data.results)[0]);
+      dataRef.current = (res.data.results)[0]
+      viewIndex.current = 1
+
+      processData()
+      
+      // console.log(res.data.results)
+                
+    }catch(e){
+      console.log(e)
+      alert('There was an error while retrieving the data')
+    }
+  }
+
+    useEffect(() => {
+      requestNewPage()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    return (
+        <div>
+          <Layout
+            data={activeData}
+            nextLink={nextLink}
+            previousLink={previousLink}
+            requestNewPage={requestNewPage}
+            viewIndex={viewIndex}
+            processData={processData}
+          />
+        </div>
+  )
+};
 
 export default App;
---
+++ src/Layout.js
@@ -0,0 +1,65 @@
+import "./Layout.css"
+
+const Layout = ({data, previousLink, nextLink, requestNewPage, viewIndex, processData}) => {
+
+  const nextPage = () => {
+    if(viewIndex.current === 1){
+      viewIndex.current = 2;
+      processData()
+    }else{
+      requestNewPage(nextLink)
+    }
+    
+  }
+  const prevPage = () => {
+    
+    if(viewIndex.current === 2){
+      viewIndex.current = 1;
+      processData()
+    }else{
+      requestNewPage(previousLink)
+    }
+  }
+
+  // console.log(data)
+  
+
+  return (
+    <div>
+      <div className ="align">
+      <table>
+        <caption>paginated random data of app users</caption>
+        <thead>
+            <tr>
+                <td># No.</td>
+                <td>Gender</td>
+                <td>Age</td>
+            </tr>
+        </thead>
+        <tbody>
+        {data.map((item, id) => (
+            <tr key={id}>
+                <td>{item.row} </td>
+                <td>{item.gender} </td>
+                <td>{item.age} </td>
+            </tr>
+        ))}
+        </tbody>
+    </table>
+    <div className="btn-group">
+        {
+          (((previousLink !== undefined) || (viewIndex.current === 2) ) && <button data-prevbtn onClick={prevPage}>Previous</button>)
+        }
+
+        {
+          ((nextLink !== undefined) && <button data-nextbtn onClick={nextPage}>Next</button>)
+        }
+        
+        <label data-pageview></label>
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default Layout
