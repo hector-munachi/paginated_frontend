5b29b710236ba178c5d06e76b02c53dcbae2b238
Hector <victornothector@gmail.com>
2022-09-09 09:59:42 +0100

    added typescript/tsconfig.json

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState, useEffect, useRef } from 'react'
+import axios from 'axios'
+import './Layout.css';
+
+function App() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
